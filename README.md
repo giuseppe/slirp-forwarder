733a9e32fe5bd29c65907bdd21c27793a0ac69d5
Giuseppe Scrivano <gscrivan@redhat.com>
2018-07-22 16:00:26 +0200

    slirp-forwarder: initial version
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

---
+++ README.md
@@ -0,0 +1,72 @@
+slirp-forwarder
+===============
+
+A tool to create a network namespace that targets rootless containers.
+
+SliRP emulates in userspace a TCP/IP stack.  It can be used to
+circument the limitation of creating TAP/TUN devices in the host
+namespace for an unprivileged user.
+
+*slirp-forwarder* runs in the host network namespace without requiring
+root privileges or a suid program to configure the network.  A TAP
+device is created inside a new network namespace.  Data is shuttled
+from the TAP device to the SLiRP stack running outside.
+
+Requirements
+============
+slirp-forwarder internally uses [libslirp](https://github.com/rd235/libslirp),
+it is required for the build.
+
+Usage
+======
+*slirp-forwarder* creates a new network namespace, configures a tap
+device and keeps a reference to it in the specified.
+
+```console
+$ slirp-forwarder /path/to/net
+```
+
+For unprivileged users, before using *slirp-forwarder* it is first
+necessary to run in a new user and mount namespace.
+
+You can use the standard `unshare(1)` tool for doing it, or if you'd
+like to get more users mapped into the namespace, you can use
+[become-root](https://github.com/giuseppe/become-root).
+
+```console
+$ unshare -mr bash # start a new bash in a mount and user namespace
+$ mount -t tmpfs tmpfs /var/run; mkdir -p /var/run/NetworkManager/
+$ touch net; slirp-forwarder net & # keep a reference in the file net
+$ nsenter --net=net dhclient -i tap0
+$ nsenter --net=net route add default tap0
+$ nsenter --net=net ifconfig -a
+lo: flags=8<LOOPBACK>  mtu 65536
+        loop  txqueuelen 1000  (Local Loopback)
+        RX packets 0  bytes 0 (0.0 B)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 0  bytes 0 (0.0 B)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+tap0: flags=67<UP,BROADCAST,RUNNING>  mtu 1500
+        inet 10.0.2.15  netmask 255.255.255.0  broadcast 10.0.2.255
+        inet6 fe80::e00f:e4ff:fe83:29cc  prefixlen 64  scopeid 0x20<link>
+        ether e2:0f:e4:83:29:cc  txqueuelen 1000  (Ethernet)
+        RX packets 2  bytes 724 (724.0 B)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 9  bytes 942 (942.0 B)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+$ nsenter --net=net wget -O- www.gnu.org
+....
+```
+
+Build
+=====
+After you have installed libslirp:
+    
+```console
+$ ./autogen.sh && ./configure && make
+```
+
+TODO
+====
+Consider the slirp implementation in QEMU.
---
+++ main.c
@@ -0,0 +1,218 @@
+/* usermode-netns: create a working network namespace without root privileges
+
+   Copyright (C) 2018 Giuseppe Scrivano <giuseppe@scrivano.org>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define _GNU_SOURCE
+#include <sched.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <arpa/inet.h> 
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <sys/select.h>
+#include <sys/mount.h>
+#include <sys/wait.h>
+
+#include <libslirp.h>
+#include <error.h>
+
+#include "cmsg.h"
+
+volatile static int do_exit = 0;
+
+static int
+tun_alloc (char *dev)
+{
+  struct ifreq ifr;
+  int fd, err;
+
+  if ((fd = open("/dev/net/tun", O_RDWR)) < 0)
+    return fd;
+
+  memset (&ifr, 0, sizeof(ifr));
+
+  ifr.ifr_flags = IFF_TAP | IFF_NO_PI; 
+  if (*dev)
+    strncpy (ifr.ifr_name, dev, IFNAMSIZ);
+
+  if ((err = ioctl (fd, TUNSETIFF, (void *) &ifr)) < 0)
+    {
+      close (fd);
+      return err;
+    }
+  strcpy (dev, ifr.ifr_name);
+  return fd;
+}
+
+static void
+sigint (int signo)
+{
+  fprintf (stderr, "exiting...\n");
+  do_exit = 1;
+}
+
+int
+main (int argc, char **argv)
+{
+  SLIRP *slirp_session = slirp_open (SLIRP_IPV4 | SLIRP_IPV6);
+  int fd, s_fd;
+  int sv[2];
+  pid_t pid;
+#define BUF_SIZE (1 << 12)
+  char *buffer;
+
+  if (argc < 2)
+    error (EXIT_FAILURE, 0, "please specify the network namespace destination file");
+  
+  if (socketpair (AF_UNIX, SOCK_DGRAM, 0, sv) < 0)
+    exit (EXIT_FAILURE);
+
+  pid = fork ();
+  if (pid < 0)
+    exit (EXIT_FAILURE);
+  if (pid == 0)
+    {
+      int sockfd;
+      struct ifreq ifr;
+      char name[IFNAMSIZ + 1];
+      struct file_t file;
+
+      file.name = "tap";
+
+      close (sv[1]);
+
+      if (unshare (CLONE_NEWNET) < 0)
+        exit (EXIT_FAILURE);
+
+      name[0] = '\0';
+      file.fd = tun_alloc (name);
+      if (file.fd < 0)
+        exit (EXIT_FAILURE);
+
+      ifr.ifr_flags = IFF_UP;
+      strcpy (ifr.ifr_name, name);
+
+      sockfd = socket (AF_INET, SOCK_DGRAM, 0);
+      if (sockfd < 0)
+        error (EXIT_FAILURE, errno, "cannot create socket");
+
+      if (ioctl (sockfd, SIOCSIFFLAGS, &ifr) < 0)
+        error (EXIT_FAILURE, errno, "cannot set device up");
+
+      close (sockfd);
+
+      if (sendfd (sv[0], file) < 0)
+        error (EXIT_FAILURE, errno, "error sending fd");
+
+      while (read (sv[0], &file.name, 1) < 0 && errno == EINTR);
+
+      if (mount ("/proc/self/ns/net", argv[1], "bind", MS_BIND | MS_SLAVE, NULL) < 0)
+        error (EXIT_FAILURE, errno, "cannot mount to %s", argv[1]);
+      fprintf (stderr, "mounted to '%s'\n", argv[1]);
+      exit (EXIT_SUCCESS);
+    }
+  else
+    {
+      int status, ret;
+      struct file_t file;
+
+      close (sv[0]);
+      file = recvfd(sv[1]);
+      if (file.fd < 0)
+        exit (EXIT_FAILURE);
+      fd = file.fd;
+
+      write (sv[1], "0", 1);
+      close (sv[1]);
+      do
+        ret = waitpid (pid, &status, 0);
+      while (ret < 0 && errno == EINTR);
+      if (!WIFEXITED (status) || WEXITSTATUS (status))
+        exit (EXIT_FAILURE);
+    }
+
+  signal (SIGINT, sigint);
+  
+  if (slirp_start (slirp_session) < 0)
+    exit (EXIT_FAILURE);
+
+  s_fd = slirp_fd (slirp_session);
+
+  buffer = malloc (BUF_SIZE);
+  if (buffer == NULL)
+    error (EXIT_FAILURE, errno, "cannot allocate buffer");
+
+  while (! do_exit)
+    {
+      fd_set fds;
+      FD_ZERO (&fds);
+      FD_SET (fd, &fds);
+      FD_SET (s_fd, &fds);
+
+      if (select ((s_fd > fd ? s_fd : fd) + 1, &fds, NULL, NULL, NULL) <= 0)
+        continue;
+
+      if (FD_ISSET (s_fd, &fds))
+        {
+          int ret;
+          ssize_t s = slirp_recv (slirp_session, buffer, BUF_SIZE);
+          if (s < 0)
+            error (EXIT_FAILURE, errno, "read from vnet");
+
+          fprintf (stderr, "got %d bytes from vnet\n", s);
+          do
+            ret = write (fd, buffer, s);
+          while (ret < 0 && errno == EINTR);
+          if (ret < 0)
+            error (EXIT_FAILURE, errno, "write to tap device");
+        }
+      if (FD_ISSET (fd, &fds))
+        {
+          ssize_t s;
+
+          do
+            s = read (fd, buffer, BUF_SIZE);
+          while (s < 0 && errno == EINTR);
+          if (s < 0)
+            error (EXIT_FAILURE, errno, "read from tap device");
+
+          fprintf (stderr, "got %d bytes from the tap device\n", s);
+
+          s = slirp_send (slirp_session, buffer, s);
+          if (s < 0)
+            error (EXIT_FAILURE, errno, "write to vnet");
+        }
+    }
+  free (buffer);
+
+  umount (argv[1]);
+  
+  slirp_close (slirp_session);
+  return 0;
+}
